56f2b51422d67973e29ae204e4f3f7a02785e3bd
fernando-utiyama <fernandoutiyama@gmail.com>
2021-10-03 19:35:43 -0300

    Refactoring

--- app/src/main/java/br/com/univesp/product/retrofit/ProductRetrofit.java
+++ app/src/main/java/br/com/univesp/product/retrofit/ProductRetrofit.java
@@ -6,10 +6,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ProductRetrofit {
 
     private final ProductService productService;
+    private static final String baseUrl = "https://univesp-campinas-pi6-back.herokuapp.com/";
 
     public ProductRetrofit() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://univesp-campinas-pi6-back.herokuapp.com/")
+                .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         productService = retrofit.create(ProductService.class);
